4a310cddf248447f681dee13b537521731e67215
ho3eintry <ho3intry@gmail.com>
2023-06-11 04:49:54 +0330

    section-02/flags

---
+++ section-02/flags/example/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/ho3eintry/go-solutions/section-02/flags"
+)
+
+func main() {
+	c := flags.Config{}
+	c.Setup()
+
+	flag.Parse()
+
+	fmt.Println(c.GetMessage())
+}
